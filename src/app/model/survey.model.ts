d8f49fe109845cf58537d4c05cb1a8b990a9bd3f
Hing Yu Lam <hlam90@my.centennialcollege.ca>
2022-11-17 10:55:43 -0500

    Change 'String' to primitive type 'string'

--- src/app/model/survey.model.ts
+++ src/app/model/survey.model.ts
@@ -2,9 +2,9 @@ export class Survey
 {
     constructor(
         public _id?: string,
-        public author?: String,
-        public name?: String,
-        public description?: String,
+        public author?: string,
+        public name?: string,
+        public description?: string,
         public start_time?: Date, 
         public end_time?: Date, 
         public questions?: Question[],
@@ -16,9 +16,9 @@ export class Question {
     constructor(
         public _id?: string,
         public question_priority?: Number, 
-        public name?: String,    
-        public type?: String,   // yes/no, true/false, radio buttons, dropdown list 
-        public options?:String[]
+        public name?: string,    
+        public type?: string,   // yes/no, true/false, radio buttons, dropdown list 
+        public options?: string[]
     ){}
 }
 
@@ -34,6 +34,6 @@ export class AnswerResponse {
     constructor(
         public _id?: string,
         public question_id?: string,
-        public options?: String[]
+        public options?: string[]
     ){}
 }
