564f1ec583125805c9f88a0862f767c46caf0818
Jerry Kuo <narikuo@gmail.com>
2022-11-19 02:35:22 -0500

    Add component survey/ report, create report page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { ActiveComponent } from './survey/active/active.component';
 import { FooterComponent } from './partials/footer/footer.component';
+import { ReportComponent } from './survey/report/report.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,8 @@ import { FooterComponent } from './partials/footer/footer.component';
     EditComponent,
     DetailComponent,
     ActiveComponent,
-    FooterComponent
+    FooterComponent,
+    ReportComponent
   ],
   imports: [
     BrowserModule,
