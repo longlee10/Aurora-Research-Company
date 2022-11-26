f62334805bcf7f80435b5ec7b9e6b08ce983881e
Hoang Long <longquan98@gmail.com>
2022-11-26 02:13:52 -0500

    adding navbar logic

--- src/app/model/auth.service.ts
+++ src/app/model/auth.service.ts
@@ -37,5 +37,5 @@ export class AuthService
   {
     return this.surveysService.logout();
   }
-
+  
 }
--- src/app/partials/navbar/navbar.component.ts
+++ src/app/partials/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/model/auth.service';
+import { User } from 'src/app/model/user.model';
 
 @Component({
   selector: 'app-nav',
@@ -9,13 +10,26 @@ import { AuthService } from 'src/app/model/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
+  user: User;
+  
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.user = new User();
   }
 
    mlogout() {
     this.authService.logout().subscribe(success => this.router.navigate(["/"]));
 
+  }
+
+  isLoggedIn(): boolean
+  {
+    const result = this.authService.authenticated;
+    if (result)
+    {
+      this.user = JSON.parse(localStorage.getItem('user'));
+    }
+    return result;
   }
 }
