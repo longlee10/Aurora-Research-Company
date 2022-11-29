a945d6e04a260b0d9b5ff20ac60e76e8b7da453b
Hing Yu Lam <hlam90@my.centennialcollege.ca>
2022-11-29 18:57:40 -0500

    Change the UI of my surveys and move the download dataset function under report

--- src/app/user/report/report.component.ts
+++ src/app/user/report/report.component.ts
@@ -4,6 +4,9 @@ import { mergeMap } from 'rxjs';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { saveAs } from "file-saver";
+import { parse } from "json2csv";
+import { Survey } from 'src/app/model/survey.model';
 
 @Component({
   selector: 'app-report',
@@ -77,6 +80,46 @@ export class ReportComponent implements OnInit {
       resultStr = "No record."
     }
     return resultStr;
+  }
+
+  downloadDataset() {
+    // Make a dataset
+    const dataset = this.createCSV(this.survey);
+    // Save as CSV file
+    const data: Blob = new Blob([dataset], {
+      type: "text/csv;charset=utf-8"
+    });
+    saveAs(data, "dataset.csv");
+  }
+  
+  private createCSV(survey: Survey) {
+    try {
+      const questions = survey?.questions ?? [];
+      // Column headers
+      const questionFields = questions.map(question => {
+        return {label: question.name ?? "", value: question._id! };
+      });
+      const fields = [{label: "Date", value: "date"}].concat(questionFields);
+
+      // Values for each answer
+      const data = survey?.answers?.map(answer => {
+       // Get date string
+       const responseDate = answer?.response_date != undefined ? new Date(answer.response_date).toLocaleDateString() : "";
+       // Add responses
+       const resultMap = questions.reduce((acc, question) => {      
+          const questionId = question._id!;  
+          const response = answer.responses?.filter(response => (response.question_id ?? "") == questionId).shift();
+          acc.set(questionId, response?.options?.join() ?? "");
+          return acc;
+        }, new Map([["date", responseDate]]));
+        return Object.fromEntries(resultMap);
+      }) ?? [];
+
+      // Get CSV
+      return parse(data, { fields });
+    } catch (err) {
+      return `Error: ${err}`;
+    }
   }
 
 }
