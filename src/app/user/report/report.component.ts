8be9dacb62562d4fd1305fb61c9e4e47623767b1
Jerry Kuo <narikuo@gmail.com>
2022-11-30 00:06:11 -0500

    Adjusted UI UX for the list, active, answer, auth, register, create survey and edit survey pages.

--- src/app/user/report/report.component.ts
+++ src/app/user/report/report.component.ts
@@ -4,6 +4,9 @@ import { mergeMap } from 'rxjs';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { Survey } from 'src/app/model/survey.model';
+import { saveAs } from "file-saver";
+import { parse } from "json2csv";
 
 @Component({
   selector: 'app-report',
@@ -26,6 +29,49 @@ export class ReportComponent implements OnInit {
 
   back(): void {
     this.location.back();
+  }
+
+  downloadDataset(survey: Survey) {
+    this.surveryService.getSurvey(survey._id!)
+    .subscribe(result => {
+      // Make a dataset
+      const dataset = this.createDataset(result);
+      // Save as CSV file
+      const data: Blob = new Blob([dataset], {
+        type: "text/csv;charset=utf-8"
+      });
+      saveAs(data, "dataset.csv");
+    })
+  }
+
+  private createDataset(survey: Survey) {
+    try {
+      const questions = survey?.questions ?? [];
+      // Column headers
+      const questionFields = questions.map(question => {
+        return {label: question.name ?? "", value: question._id! };
+      });
+      const fields = [{label: "Date", value: "date"}].concat(questionFields);
+
+      // Values for each answer
+      const data = survey?.answers?.map(answer => {
+       // Get date string
+       const responseDate = answer?.response_date != undefined ? new Date(answer.response_date).toLocaleDateString() : "";
+       // Add responses
+       const resultMap = questions.reduce((acc, question) => {      
+          const questionId = question._id!;  
+          const response = answer.responses?.filter(response => (response.question_id ?? "") == questionId).shift();
+          acc.set(questionId, response?.options?.join() ?? "");
+          return acc;
+        }, new Map([["date", responseDate]]));
+        return Object.fromEntries(resultMap);
+      }) ?? [];
+
+      // Get CSV
+      return parse(data, { fields });
+    } catch (err) {
+      return `Error: ${err}`;
+    }
   }
 
   generateQuestionResult(question:any): string {
